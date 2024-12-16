819e8818d93e4eed3c0aa6555643edfc6646598e
AndreyKhramykh <khramykhandrey@gmail.com>
2024-12-17 00:05:31 +0200

    remove unnecessary logic from day 4

--- Day4/task1.js
+++ Day4/task1.js
@@ -16,14 +16,11 @@ function getResult(arg) {
 	let keyWord = 'XMAS'
 	let result = 0
 
-	function searchingWords(startLetter, i, j) {
-		let searchingWord = ''
-		if (startLetter == 'X') searchingWord = keyWord
-		if (startLetter == 'S') searchingWord = keyWord.split('').reverse().join('')
+	function searchingWords(i, j) {
 		let hitCounter = 0
 		for (let d = 0; d < directions.length; d++) {
 			let currentWord = ''
-			for (let l = 0; l < searchingWord.length; l++) {
+			for (let l = 0; l < keyWord.length; l++) {
 				let stepY = directions[d].stepY * l + i
 				let stepX = directions[d].stepX * l + j
 				if (
@@ -36,17 +33,17 @@ function getResult(arg) {
 					currentWord += currentLetter
 				}
 			}
-			if (currentWord == searchingWord) {
+			if (currentWord == keyWord) {
 				hitCounter++
 			}
 		}
-		return hitCounter / 2
+		return hitCounter
 	}
 
 	for (let i = 0; i < data.length; i++) {
 		for (let j = 0; j < data[0].length; j++) {
-			if (data[i][j] == 'X' || data[i][j] == 'S') {
-				result += searchingWords(data[i][j], i, j)
+			if (data[i][j] == 'X') {
+				result += searchingWords(i, j)
 			}
 		}
 	}
